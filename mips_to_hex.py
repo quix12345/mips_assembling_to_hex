000cca0884169fb7cbaba2a0450e49e7457029b6
Quix <qxdnfsy@126.com>
2021-12-10 16:28:54 +0000

    sys.arg supported

--- mips_to_hex.py
+++ mips_to_hex.py
@@ -3,8 +3,17 @@ Copyright under MIT license
 Author: Quix, Josiah
 Imperial College London
 """
+import getopt
+import os
 
 import execjs
+
+copyright = """
+===============================
+Copyright under MIT license
+Author: Quix, Josiah
+Imperial College London
+==============================="""
 
 
 class MIPS_Assembler(object):
@@ -72,7 +81,7 @@ class MIPS_Assembler(object):
                     ram_list.append(byte)
         return ins_list, ram_list
 
-    def process_file(self, src_path="./src/source_code.txt", mode="both"):
+    def process_file(self, src_path="./src/source_code.txt", out_path="./out/output.txt", mode="byte"):
         ins_list = []
         f = open(src_path, "r", encoding='utf-8')
         lines = f.readlines()
@@ -91,11 +100,14 @@ class MIPS_Assembler(object):
             print("%s (%s)" % (line, code_in_hex))
             ins_list.append(code_in_hex)
         f.close()
-        if mode == "both":
-            f = open("./out/output.txt", "w", encoding="utf-8")
-            ram = open("./out/ram.txt", "w", encoding="utf-8")
+        if mode == "hex":
+            f = open(out_path, "w", encoding="utf-8")
             for ins in ins_list:
                 f.write(ins + "\n")
+            f.close()
+        elif mode == "byte":
+            ram = open(out_path, "w", encoding="utf-8")
+            for ins in ins_list:
                 if "[Error]" in ins:
                     for i in range(4):
                         ram.write("[Error]" + "\n")
@@ -103,10 +115,50 @@ class MIPS_Assembler(object):
                     bytes = self.hex_to_bytes(ins)
                     for byte in bytes:
                         ram.write(byte + "\n")
-            f.close()
             ram.close()
 
 
-endian_reverse = True
-assembler = MIPS_Assembler(endian_reverse)
-assembler.process_file()
+if __name__ == '__main__':
+    import sys
+
+    print(copyright)
+    endian = True
+    mode = "byte"
+    argv = sys.argv[1:]
+    inputfile = "./src/source_code.txt"
+    outputfile = "./out/output.txt"
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:m:e", ["ifile=", "ofile="])
+    except getopt.GetoptError:
+        print('mips_to_hex.py -i <inputfile> -o <outputfile> -m <mode> -e <endian>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('mips_to_hex.py -i <inputfile> -o <outputfile> -m <mode> -e <endian>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+            if not os.path.exists(inputfile):
+                print("Error! input file doesnt exist!")
+                sys.exit(3)
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+            outputdir = "/".join(outputfile.split("/")[:-1])
+            if not os.path.exists(outputdir):
+                print("Error! output file doesnt exist!")
+                sys.exit(4)
+        elif opt in ("-m", "--mode"):
+            mode = arg
+            if not mode in ["byte", "hex"]:
+                mode = "byte"
+                print("Unknown mode! Convert mode to byte instead")
+        elif opt in ("-e", "--endian"):
+            endian = str(arg).lower() == "true"
+            print("Endian reverse is " + str(endian))
+    print("===============================\nStart converting...")
+    try:
+        endian_reverse = True
+        assembler = MIPS_Assembler(endian)
+        assembler.process_file(src_path=inputfile, out_path=outputfile, mode=mode)
+    except:
+        print("Undefined error happened!\nI give up!")
--- readme.md
+++ readme.md
@@ -1,10 +1,34 @@
-# MIP to HEX complier
+# MIP32 Assembler
 
 ### Environment
-For python, pip install pyexecjs before exxcute
+1. install python3 and nodejs, add them to system PATH(/bin)
+2. For python, run "pip install pyexecjs" before execute
 
-### Tutorial
-1. Put the source assebling code in source_code.text
+### For Use
+##### Direct use
+1.run mips_to_hex.py, it will assembling the code in "src/source_code.txt" to "out" by default
+##### By command line
+2.run python mips_to_hex.py -i \<inputfile> -o \<outputfile> -m \<mode> -e \<endian>
+* -i: input file name
+* -o: output file name
+* -m: output mode(hex or byte, default by byte)
+* -e: whether reverse the endian system(default by true)
+
+### Example code for test
+- addiu $v0, $zero, 0x34
+- addiu $v0, $v0, 0x02
+- jr $zero
+
+### Tutorial on MIP32 assembling
+1. Support most of the MIP32 ISA
+2. Lower case or Upper case doesn't matter (lw=LW,ADDIU=addiu)
+3. Comment by "// comment" or "(comment)"
+4. immediate values should be writing in hex(e.g. 0xFFFF) but it can be in dec as well
+5. space after a comma was appreciated, but the assembler will format it automatically
+6. see http://mipsconverter.com/opcodes.html for MIPS assembling tutorial
+
+##### Old Tutorial (obselete)
+1. Put the source assebling code in source_code.txt
 2. Run mips_to_hex.py
 3. Get result in ram.txt and output.txt
 
